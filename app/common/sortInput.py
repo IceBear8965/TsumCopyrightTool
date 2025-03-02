f0f8f2dc5b3c5d1f23e104a026e1fa0a3ba6f5df
IceBear8965 <icebee088@gmail.com>
2025-03-02 20:54:50 +0200

    Added copyright in each file

--- app/common/sortInput.py
+++ app/common/sortInput.py
@@ -1,3 +1,16 @@
+'''
+Copyright (C) 2025 IceBear8965
+
+This program is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details.
+'''
+
 def sortInput(data, filters, orderfilter):
     data = [d.replace("•", "") for d in data]
     data = [d.strip() for d in data]
